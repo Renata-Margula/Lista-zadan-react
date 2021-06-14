52b3813a3449b4e86dbf4fd9f7446c5ebc9aae80
Renata Margula <reniutaaa@gmail.com>
2021-06-14 21:04:55 +0200

    toggleTaskDone interaction

--- src/App.js
+++ src/App.js
@@ -21,6 +21,15 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
+      return task;
+    }));
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -29,7 +38,10 @@ function App() {
         body={<Form />} />
       <Section
         title="Lista zadań"
-        body={<Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} />}
+        body={<Tasks tasks={tasks}
+          hideDone={hideDone}
+          removeTask={removeTask}
+          toggleTaskDone={toggleTaskDone} />}
         extraHeaderContent={
           <Buttons
             tasks={tasks}
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone, removeTask }) => (
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
     <ul className="list">
         {tasks.map(task => (
             <li
                 key={task.id}
                 className={`list__tasks${task.done && hideDone ? " list__item--hidden" : ""}`}>
-                <button className="list__button list__button--done">
+                <button
+                    className="list__button list__button--done"
+                    onClick={() => toggleTaskDone(task.id)}
+                >
                     {task.done ? "✓" : ""}
                 </button>
                 <span className={`list__item${task.done ? " list__item--done" : ""}`}>
