87ecde536fec945a3b57d232eeff2a1e3203ec7f
Renata Margula <reniutaaa@gmail.com>
2021-06-29 20:57:50 +0200

    h2 changed for header

--- src/Section/index.js
+++ src/Section/index.js
@@ -3,9 +3,9 @@ import "./style.css";
 
 const Section = ({ title, body, extraHeaderContent }) => (
   <section className="section">
-    <h2 className="section__header">{title}
+    <header className="section__header">{title}
       {extraHeaderContent}
-    </h2>
+    </header>
     <div className="section__body">
       {body}
     </div>
