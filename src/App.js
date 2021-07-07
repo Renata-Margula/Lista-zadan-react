b0261f980553fc0e79fbaadac941c724943f342b
Renata Margula <reniutaaa@gmail.com>
2021-07-07 21:36:38 +0200

    deleted sample tasks

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ import Container from "./Container";
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem('tasks')) || [
-    { id: 1, content: "przejść na Reacta", done: false },
-    { id: 2, content: "zjeść obiad", done: true },
-  ]);
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem('tasks')) || []);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
